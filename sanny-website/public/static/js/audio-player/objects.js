b37eb3066c600ce21fc8802cb1b4aca3dbfe252a
kikisal <57672398+kikisal@users.noreply.github.com>
2023-04-09 22:56:25 +0200

    Add files via upload

--- sanny-website/public/static/js/audio-player/objects.js
+++ sanny-website/public/static/js/audio-player/objects.js
@@ -6,88 +6,83 @@
  */
 Base.__setModule('AudioPlayer', {
     Widgets: {
-        Ball: function() {
+        Ball: function () {
             SimpleGFX.SceneObject.call(this);
 
             const { Ball } = AudioPlayer.Widgets;
 
-            this.size  = Ball.DEFAULT_SIZE;
+            this.size = Ball.DEFAULT_SIZE;
             this.color = Ball.DEFAULT_COLOR;
-		
-			this.toResume = false;
-        
-            this.update = function() {
+
+            this.toResume = false;
+
+            this.update = function () {
 
                 if (SimpleGFX.SceneObject.update.call(this))
                     return;
 
                 const audio = this.getParent().getAudioPlayer();
-				
-				/* mouse handling. */
-				const mouse = this.getRendererContext().getMouse();
-				
-				if (mouse.down)
-				{
-					if (audio.isPlaying())
-					{
-						this.toResume = true;
-						audio.pause();
-					}
-					
-					const matrix = this.getRendererContext().getCanvasContext().getTransform();
-					
-					this._position.x = mouse.x - matrix.e;
-					if (this._position.x < 0)
-						this._position.x = 0;
-					
-					if (this._position.x > this.getParent().width)
-						this._position.x = this.getParent().width;
-					
-					audio.setCursor( (this._position.x / this.getParent().width) * audio.getDuration() );
-					
-				} else {
-					if (this.toResume)
-					{
-						audio.play();
-						this.toResume = false;
-					}
-					
-					
-					if (audio.isPlaying())
-					{
-						const currTime = audio.getCurrentTime() / audio.getDuration();
-						this._position.x = currTime * this.getParent().width;
-					}
-					
-					if (audio.fullyPlayed())
-					{
-						audio.pause();
-						audio.setCursor(0);
 
-						this.moveTo({x: 0, y: 0}, 1000);
+                /* mouse handling. */
+                const mouse = this.getRendererContext().getMouse();
 
-						setTimeout(function() { if(!audio.isPlaying()) audio.stop(); }.bind(this), 5000);
+                if (mouse.down) {
+                    if (audio.isPlaying()) {
+                        this.toResume = true;
+                        audio.pause();
+                    }
 
-						this.getParent().getAudioPlayer().onFullyPlayed();
-					}
-				}
-				
+                    const matrix = this.getRendererContext().getCanvasContext().getTransform();
+
+                    this._position.x = mouse.x - matrix.e;
+                    if (this._position.x < 0)
+                        this._position.x = 0;
+
+                    if (this._position.x > this.getParent().width)
+                        this._position.x = this.getParent().width;
+
+                    audio.setCursor((this._position.x / this.getParent().width) * audio.getDuration());
+
+                } else {
+                    if (this.toResume) {
+                        audio.play();
+                        this.toResume = false;
+                    }
+
+
+                    if (audio.isPlaying()) {
+                        const currTime = audio.getCurrentTime() / audio.getDuration();
+                        this._position.x = currTime * this.getParent().width;
+                    }
+
+                    if (audio.fullyPlayed()) {
+                        audio.pause();
+                        audio.setCursor(0);
+
+                        this.moveTo({ x: 0, y: 0 }, 1000);
+
+                        setTimeout(function () { if (!audio.isPlaying()) audio.stop(); }.bind(this), 5000);
+
+                        this.getParent().getAudioPlayer().onFullyPlayed();
+                    }
+                }
+
             };
 
-            this.draw = function() {
+            this.draw = function () {
                 const ctx = this.getRendererContext().getCanvasContext();
                 ctx.fillStyle = this.color;
-        
+
                 ctx.beginPath();
                 ctx.arc(0, 0, this.size, 0, 2 * Math.PI, false);
                 ctx.fill();
                 ctx.closePath();
             }
         },
-    
-        TimeLine: function() {
+
+        TimeLine: function () {
             SimpleGFX.SceneObject.call(this);
-    
+
             const { TimeLine, Ball } = AudioPlayer.Widgets;
 
             this.size = TimeLine.DEFAULT_SIZE;
@@ -101,48 +96,46 @@ Base.__setModule('AudioPlayer', {
             this.ball = SimpleGFX.ObjectCreator(Ball);
 
             this.push(this.ball);
-            
-            this.ball.setPosition({x: 0, y: 0});
 
-            this.setAudioPlayer = function(audioPlayer) {
+            this.ball.setPosition({ x: 0, y: 0 });
+
+            this.setAudioPlayer = function (audioPlayer) {
                 this._audioPlayer = audioPlayer;
             };
 
-            this.getAudioPlayer = function() {
+            this.getAudioPlayer = function () {
                 return this._audioPlayer;
             };
 
-            this.close = function(onCloseListener) {
-                
+            this.close = function (onCloseListener) {
+
                 this.moveTo(this.closePosition, 2000, onCloseListener);
             };
 
-            this.open = function() {
+            this.open = function () {
                 this.moveTo(this.openPosition, 6000);
             };
 
 
-            this.init = function() {
+            this.init = function () {
                 this.width = this.getRendererContext().getCanvasWidth() * this.portion;
             };
 
-            this.draw = function() {
+            this.draw = function () {
                 const ctx = this.getRendererContext().getCanvasContext();
                 ctx.fillStyle = this.color;
                 // this.getRendererContext().fillRect()
-    
+
                 ctx.fillRect(0, -this.size / 2, this.width, this.size);
             }
-        },    
+        },
     },
 
-    GenTimeline: function()
-    {
+    GenTimeline: function () {
         return SimpleGFX.ObjectCreator.spawn(AudioPlayer.Widgets.TimeLine);
     },
 
-    GenBall: function()
-    {
+    GenBall: function () {
         return SimpleGFX.ObjectCreator.spawn(AudioPlayer.Widgets.Ball);
     }
 });
