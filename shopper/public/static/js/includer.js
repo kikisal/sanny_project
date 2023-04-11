16a495e86b73e1ccd1b3231fa0a44aa174d6ed10
kikisal <57672398+kikisal@users.noreply.github.com>
2023-04-11 17:09:10 +0200

    Add files via upload

---
+++ shopper/public/static/js/includer.js
@@ -0,0 +1,45 @@
+/**
+ * Exclusive script by weeki. 
+ * 
+ * @author weeki
+ * 
+ */
+(() => {
+    let scriptsLoaded = [];
+
+    Base.__setModule('Includer', {
+        scriptsLoaded: [],
+        
+        isLoaded: function(script) {
+            return Includer.scriptsLoaded.includes(script);
+        },
+
+        include: function(scripts) {
+            return new Promise((res, rej) => {
+                let loaded = 0;
+                
+                for (const script of scripts) {
+                    if (Includer.isLoaded(script))
+                        res();
+                    else
+                    {
+                        const scr = document.createElement('script');
+                        scr.src = 'ScriptUri' in Includer ? Includer.ScriptUri + '/' + script : '/' + script + '?t=' + new Date().getMilliseconds();
+                        scr.async = false;
+        
+                        document.head.appendChild(scr);
+        
+                        scr.addEventListener('load', () => {
+                            loaded++;
+    
+                            Includer.scriptsLoaded.push(script);
+                            
+                            if (loaded >= scripts.length)
+                                res();
+                        });
+                    }
+                }
+            });
+        }
+    });
+})();
